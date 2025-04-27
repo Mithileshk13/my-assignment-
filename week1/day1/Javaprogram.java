dae912926dc59b2084bb6c057d224d1313242d69
Mithileshk13 <137436844+Mithileshk13@users.noreply.github.com>
2025-04-27 14:33:55 +0530

    Add files via upload
    Week 1

---
+++ week1/day1/Javaprogram.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+
+//access modifiers /keyword /classname
+public class Javaprogram {
+	public static void main(String[] args) {
+	
+		System.out.println("welcome to test leaf");
+	}
+}
